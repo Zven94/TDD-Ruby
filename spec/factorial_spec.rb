d5347eeee0b0b4e950c3961a8becda4098e19426
Nico Emiliano <nicobolanez@gmail.com>
2023-09-07 15:16:05 -0300

    Update factorial_spec.rb

--- spec/factorial_spec.rb
+++ spec/factorial_spec.rb
@@ -8,7 +8,7 @@ describe 'When testing reverse method of Solver Class' do
     expect(solver.factorial(5)).to eq(120)
   end
 
-  it 'should return the factorial of number: 5' do
+  it 'should return the factorial of number: 10' do
     expect(solver.factorial(10)).to eq(3_628_800)
   end
 
