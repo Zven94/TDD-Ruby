872edf792e2f5bd066cf81ab87ec5a94542af2d9
Nicolas Bolañez <Zven94>
2023-09-07 15:10:39 -0300

    add test for negative numbers

--- spec/factorial_spec.rb
+++ spec/factorial_spec.rb
@@ -12,7 +12,11 @@ describe 'When testing reverse method of Solver Class' do
     expect(solver.factorial(10)).to eq(3_628_800)
   end
 
-  it 'should return the factorial of number: 5' do
-    expect(solver.factorial(8)).to eq(40_320)
+  it 'should return the factorial of number: 0' do
+    expect(solver.factorial(0)).to eq(1)
+  end
+
+  it 'should raise an exception for negative input' do
+    expect { solver.factorial(-5) }.to raise_error(ArgumentError, 'Input must be a non-negative integer')
   end
 end
